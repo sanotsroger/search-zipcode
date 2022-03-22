16e9ee8547f1a5bd0bb7db19f009a663d6612889
Roger Santos <rogerioantoniodossantos@outlook.com>
2022-03-22 14:13:59 -0300

    Defined search class

--- example.php
+++ example.php
@@ -1,1 +1,9 @@
 <?php
+
+require 'vendor/autoload.php';
+
+use Sanotsroger\SearchZipcode\Search;
+
+$search = new Search();
+
+print_r($search->getAdressbyZipcode('00100000'));
---
+++ src/Search.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sanotsroger\SearchZipcode;
+
+class Search
+{
+    public function getAdressByZipcode(string $zipcode): array
+    {
+        return [];
+    }
+}
