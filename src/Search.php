a0d20b3c66a8b68ea854655513002c55a8e963d9
Roger Santos <rogerioantoniodossantos@outlook.com>
2022-03-23 20:45:45 -0300

    Add php code sniffer config

--- src/Search.php
+++ src/Search.php
@@ -10,7 +10,7 @@ class Search
     {
         $zipcode_numbers = preg_replace('/[^0-9]/im', '', $zipcode);
 
-        $result = file_get_contents($this::API_URL.'/'.$zipcode_numbers.'/json');
+        $result = file_get_contents($this::API_URL . '/' . $zipcode_numbers . '/json');
 
         $adress = json_decode($result, true);
 
